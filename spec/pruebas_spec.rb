baca7302bbe1b8d07edd308c9ba57177de89884e
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2021-01-26 19:06:56 +0000

    prueba

---
+++ spec/pruebas_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pruebas do
+  it "has a version number" do
+    expect(Pruebas::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
